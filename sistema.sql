08a1efdb55f9d5bf1050b7c4f02bca198683fabf
Maria Eduarda dos Santos Rosa <maria.e.santosr13@gmail.com>
2024-11-26 09:42:19 -0300

    Criação do banco de dados

---
+++ sistema.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE sistema;
+USE sistema;
+CREATE TABLE usuarios (
+id INT AUTO_INCREMENT PRIMARY KEY,
+usuario VARCHAR(50) NOT NULL,
+senha VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE fornecedores (
+id INT AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR (100) NOT NULL,
+email VARCHAR (100),
+telefone VARCHAR (20)
+);
+
+CREATE TABLE produtos (
+id INT AUTO_INCREMENT PRIMARY KEY,
+fornecedor_id INT,
+nome VARCHAR (100) NOT NULL,
+descricao TEXT,
+preco DECIMAL (10, 2),
+FOREIGN KEY (fornecedor_id) REFERENCES fornecedores (id)
+);
+
+INSERT INTO usuarios (usuario, senha) VALUES ('user1', MD5('123'));
+INSERT INTO usuarios (usuario, senha) VALUES ('user2', MD5('123'));
+INSERT INTO usuarios (usuario, senha) VALUES ('user3', MD5('123'));
+INSERT INTO usuarios (usuario, senha) VALUES ('user4', MD5('123'));
